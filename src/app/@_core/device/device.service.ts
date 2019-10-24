dee26368cbc8c9e9a9e40d19073e4f8b85dbb59e
mactepwar <mactepwar@outlook.com>
2019-10-24 14:05:34 +0300

    refaktored code

--- src/app/@_core/device/device.service.ts
+++ src/app/@_core/device/device.service.ts
@@ -1,46 +1,75 @@
 import { Injectable } from '@angular/core';
 import { Device } from 'src/app/@_models/device';
 import { ApiService } from 'src/app/@_shared/api/api.service';
-import { Sensor } from 'src/app/@_models/sensor';
+import { Sensor, DataOfSensor } from 'src/app/@_models/sensor';
 import { SensorData } from 'src/app/@_models/ResponseModels/sensor-data';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+/** Сервис для управления устройствами и их данными */
 export class DeviceService {
-  selectedDevice: Device = null;
+  selectedDevice: BehaviorSubject<Device> = new BehaviorSubject<Device>(null);
   deviceList: Array<Device> = new Array<Device>();
   lastSensorData: Array<SensorData> = Array<SensorData>();
   lastPositions: Array<{ title: string, serial: string, datetime: number, position: { lat: string, lng: string } }> = new Array<{ title: string, serial: string, datetime: number, position: { lat: string, lng: string } }>();
 
+  /** Содержит методы и свойства для управления устройствами и их данными */
   constructor(private apiService: ApiService) {
 
   }
 
-  async getLastPositions() {
-    return await this.apiService.SendComand<any>("api/devices/GetLastPositionFromDevices").toPromise();
+  /**
+   * Получает последние позиции всех устройств их вермя, и записывает в lastPositions 
+   */
+  getLastPositions() {
+    this.apiService.SendComand<any>("api/devices/GetLastPositionFromDevices").subscribe(devices => {
+      devices.forEach(device => {
+        this.lastPositions.push({ 
+          title: device.valueid.slice(0, device.valueid.indexOf('.')), 
+          serial: device.valueid.slice(0, device.valueid.indexOf('.')), 
+          datetime: device.datetime, 
+          position: { 
+            lat: device.value[0], 
+            lng: device.value[1] 
+          } 
+        });
+      });
+    });
   }
 
-  async getDataFromSensor(serial: string, sensor_id: string, date_start: number = null, date_finish: number = null) {
-    let res = await this.apiService.SendComand<any>(`api/devices/GetDataFromSensor/${serial}/${sensor_id}`, [{ "DateTimeBegin": date_start }, { "DateTimeFinish": date_finish }]).toPromise();
-    return res;
-  }
+  // async getDataFromSensor(serial: string, sensor_id: string, date_start: number = null, date_finish: number = null) {
+  //   let res = await this.apiService.SendComand<any>(`api/devices/GetDataFromSensor/${serial}/${sensor_id}`, [{ "DateTimeBegin": date_start }, { "DateTimeFinish": date_finish }]).toPromise();
+  //   return res;
+  // }
 
-  getDataFromSensor2(serial: string, sensor_id: string, date_start: number = null, date_finish: number = null) {
-    return this.apiService.SendComand<any>(`api/devices/GetDataFromSensor/${serial}/${sensor_id}`, [{ "DateTimeBegin": date_start }, { "DateTimeFinish": date_finish }]);
-  }
+  // getDataFromSensor2(serial: string, sensor_id: string, date_start: number = null, date_finish: number = null) {
+  //   return this.apiService.SendComand<any>(`api/devices/GetDataFromSensor/${serial}/${sensor_id}`, [{ "DateTimeBegin": date_start }, { "DateTimeFinish": date_finish }]);
+  // }
 
+  /**
+   * Получает список всех устройств и сохраняет их в devices
+   */
   getDeviceList() {
-    return this.apiService.SendComand<any>("api/devices");
+    this.apiService.SendComand<any>("api/devices").subscribe(devices => {
+      devices.forEach((device: Device) => {
+        this.deviceList.push(device);
+      });
+    });
   }
 
-  getCountDataFromSensor(serial: string, sensor_id: string, date_start: number = null, date_finish: number = null) {
-    return this.apiService.SendComand<any>(`api/devices/GetCountDataFromSensor/${serial}/${sensor_id}`, [{ "DateTimeBegin": date_start }, { "DateTimeFinish": date_finish }]);
-  }
+  // getCountDataFromSensor(serial: string, sensor_id: string, date_start: number = null, date_finish: number = null) {
+  //   return this.apiService.SendComand<any>(`api/devices/GetCountDataFromSensor/${serial}/${sensor_id}`, [{ "DateTimeBegin": date_start }, { "DateTimeFinish": date_finish }]);
+  // }
 
+  /**
+   * Получает последние данные по пиндексу массива позций (по идексу позиции забирает дату, которую считает последней)
+   * @param indexPos Индекс позиции 
+   */
   getLastDataFromSensors(indexPos: number) {
-    let finish_datetime = this.selectedDevice.sensors.filter(f => f.type === "position")[0].values[indexPos].datetime;
-    this.apiService.SendComand<any>(`api/devices/GetLastDataFromSensors/${this.selectedDevice.serial}`, finish_datetime).subscribe(lastData => {
+    let finish_datetime = this.selectedDevice.getValue().sensors.filter(f => f.type === "position")[0].values[indexPos].datetime;
+    this.apiService.SendComand<any>(`api/devices/GetLastDataFromSensors/${this.selectedDevice.getValue().serial}`, finish_datetime).subscribe(lastData => {
       this.lastSensorData = [];
       lastData.forEach((element: {
         sensor_id: string,
@@ -51,5 +80,28 @@ export class DeviceService {
         this.lastSensorData.push(element);
       });
     });
+  }
+
+  /**
+   *  Устанавливает устройство как выбранное
+   * @param device_id Индекс устройства с переменной devices
+   */
+  selectDevice(device_id) {
+    let serial = this.lastPositions[device_id].serial;
+    this.apiService.SendComand<Device>(`api/devices/test_get_devices/${serial}`).subscribe(
+      devices => {
+        let device: Device = devices[0];
+        this.apiService.SendComand(`api/devices/test_get_sensors_for_device/${device.serial}/position`).subscribe((res: Sensor) => {
+          device.sensors = new Array<Sensor>();
+          device.sensors.push(res[0]);
+          this.apiService.SendComand(`api/devices/test_get_data_for_sensor/${device.serial}`, device.sensors[0].sensor_id).subscribe((values: Array<DataOfSensor>) => {
+            device.sensors[0].values = new Array<DataOfSensor>();
+            device.sensors[0].values = values;
+            this.selectedDevice.next(device);
+            this.getLastDataFromSensors(this.selectedDevice.getValue().sensors.filter(f => f.type === "position")[0].values.length - 1);
+          });
+        });
+      }
+    )
   }
 }
--- src/app/@_pages/map-dashboard/map-dashboard.component.ts
+++ src/app/@_pages/map-dashboard/map-dashboard.component.ts
@@ -1,84 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceService } from 'src/app/@_core/device/device.service';
-import { Sensor, DataOfSensor } from 'src/app/@_models/sensor';
-import { Device } from 'src/app/@_models/device';
-import { ApiService } from 'src/app/@_shared/api/api.service';
 
 @Component({
   selector: 'app-map-dashboard',
   templateUrl: './map-dashboard.component.html',
   styleUrls: ['./map-dashboard.component.scss']
 })
+/** Компонент-страничка MapDashboardComponent */
 export class MapDashboardComponent implements OnInit {
 
   deviceListIsVisible: boolean = false;
   countSteps: number = 0;
 
-  constructor(private deviceService: DeviceService, private api: ApiService) { }
-
-  async ngOnInit() {
-    let lastPos = await this.deviceService.getLastPositions();
-
-    lastPos.forEach(device => {
-      this.deviceService.lastPositions.push({ title: device.valueid.slice(0, device.valueid.indexOf('.')), serial: device.valueid.slice(0, device.valueid.indexOf('.')), datetime: device.datetime, position: { lat: device.value[0], lng: device.value[1] } });
-    });
-
-    this.deviceService.getDeviceList().subscribe(devices => {
-      devices.forEach((device:Device) => {
-        this.deviceService.deviceList.push(device);
-      });
-    });
-  }
-
-  async onSelectDevice(device_id) {
-    this.deviceListIsVisible = false;
-    let serial = this.deviceService.lastPositions[device_id].serial;
-
-    this.api.SendComand<Device>(`api/devices/test_get_devices/${serial}`).subscribe(
-      devices => {
-        let device: Device = devices[0];
-        this.api.SendComand(`api/devices/test_get_sensors_for_device/${device.serial}/position`).subscribe((res: Sensor) => {
-          device.sensors = new Array<Sensor>();
-          device.sensors.push(res[0]);
-          this.api.SendComand(`api/devices/test_get_data_for_sensor/${device.serial}`, device.sensors[0].sensor_id).subscribe((values: Array<DataOfSensor>) => {
-            device.sensors[0].values = new Array<DataOfSensor>();
-            device.sensors[0].values = values;
-            this.deviceService.selectedDevice = device;
-            this.countSteps = this.deviceService.selectedDevice.sensors.filter(f => f.type === "position")[0].values.length - 1;
-            this.deviceService.getLastDataFromSensors(this.countSteps);
-          });
-        });
+  constructor(private deviceService: DeviceService) { 
+    this.deviceService.selectedDevice.subscribe(selectedDevice => {
+      if (selectedDevice){
+        this.countSteps = selectedDevice.sensors.filter(f => f.type === "position")[0].values.length - 1;
       }
-    );
-
-    // this.deviceService.getDataFromSensor2(serial, "position").subscribe((res:Array<SensorData>) => {
-
-    //   this.currentRoad = new Device(res[0].valueid.slice(0, res[0].valueid.indexOf('.')),res[0].valueid.slice(0, res[0].valueid.indexOf('.')));
-    //   res.forEach(device => {
-    //     //console.log(device.valueid);
-    //     if (parseFloat(device.value[0]) > 0.0 || parseFloat(device.value[1]) > 0.0) {
-    //       this.currentRoad["asd"] = "123";
-    //       //this.currentRoad.Sensors.push(new Sensor(device.datetime,device.valueid.slice(device.valueid.indexOf('.')) + 1,device.value));
-    //       // this.currentRoad.title = device.valueid.slice(0, device.valueid.indexOf('.'));
-    //       // this.currentRoad.serial = device.valueid.slice(0, device.valueid.indexOf('.'));
-    //       // this.currentRoad.datetime = device.datetime;
-    //       // this.currentRoad.positions.push({ lat: parseFloat(device.value[0]), lng: parseFloat(device.value[1]) } );
-    //     }
-    //   });
-    //   this.deviceService.getCountDataFromSensor(serial, "position").subscribe(res => {
-    //     this.countSteps = res - 1;
-    //     this.getLastDataFromSensors(this.countSteps);
-    //   });
-    //   this.selectedDeviceSerial = serial;
-    //   console.log(this.currentRoad);
-    // });
-
+    });
   }
 
+  ngOnInit() {
+    this.deviceService.getLastPositions();
+    this.deviceService.getDeviceList();
+  }
+
+  /**
+   * Происходит при выборе устройства
+   * @param device_id Индекс устройства
+   */
+  onSelectDevice(device_id) {
+    this.deviceListIsVisible = false;
+    this.deviceService.selectDevice(device_id);
+  }
+
+  /**
+   * Показывает/скрывает список устройств
+   */
   toggleDeviceList() {
     this.deviceListIsVisible = !this.deviceListIsVisible;
   }
 
+  /**
+   * Происходит при изменении горизонтального range input
+   * @param value Значение горизонтального range input
+   */
   onChangeRangeHistory(value: number) {
     this.deviceService.getLastDataFromSensors(value);
   }
